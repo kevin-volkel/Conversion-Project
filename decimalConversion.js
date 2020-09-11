4f0aa5b04d77bf83dd48e7d3ea0763cf70409d5b
Kevin <kevinator900@gmail.com>
2020-09-11 13:00:42 -0700

    Commiting so I can pull, no major changes

--- decimalConversion.js
+++ decimalConversion.js
@@ -1,46 +1,47 @@
 let decimal = Number(prompt("Type any whole number."));
-let binary;
+
 
 let hexa1 = Math.floor(decimal/16);
-let remainder1 = decimal%16;
-let remainder2 = Math.floor(hexa1%16); 
-if(remainder1 == 10){
-    remainder1 = "A";
+let hexRemainder1 = decimal%16;
+let hexRemainder2 = Math.floor(hexa1%16); 
+if(hexRemainder1 == 10){
+    hexRemainder1 = "A";
 }
-if(remainder1 == 11){
-    remainder1 = "B";
+if(hexRemainder1 == 11){
+    hexRemainder1 = "B";
 }
-if(remainder1 == 12){
-    remainder1 = "C";
+if(hexRemainder1 == 12){
+    hexRemainder1 = "C";
 }
-if(remainder1 == 13){
-    remainder1 = "D";
+if(hexRemainder1 == 13){
+    hexRemainder1 = "D";
 }
-if(remainder1 == 14){
-    remainder1 = "E";
+if(hexRemainder1 == 14){
+    hexRemainder1 = "E";
 }
-if(remainder1 == 15){
-    remainder1 = "F";
+if(hexRemainder1 == 15){
+    hexRemainder1 = "F";
 }
-if(remainder2 == 10){
-    remainder2 = "A";
+if(hexRemainder2 == 10){
+    hexRemainder2 = "A";
 }
-if(remainder2 == 11){
-    remainder2 = "B";
+if(hexRemainder2 == 11){
+    hexRemainder2 = "B";
 }
-if(remainder2 == 12){
-    remainder2 = "C";
+if(hexRemainder2 == 12){
+    hexRemainder2 = "C";
 }
-if(remainder2 == 13){
-    remainder2 = "D";
+if(hexRemainder2 == 13){
+    hexRemainder2 = "D";
 }
-if(remainder2 == 14){
-    remainder2 = "E";
+if(hexRemainder2 == 14){
+    hexRemainder2 = "E";
 }
-if(remainder2 == 15){
-    remainder2 = "F";
+if(hexRemainder2 == 15){
+    hexRemainder2 = "F";
 }
 
 
-console.log(`The number in hexadecimal is ${remainder2}${remainder1}`)
+console.log(`The number in hexadecimal is ${hexRemainder2}${hexRemainder1}`)
 
+let binary1 = binaryRemainder1 + binaryRemainder2 + binaryRemainder3 + binaryRemainder4
